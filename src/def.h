e42cd7ee28baca8f173d5848fcfcdf2feaa5880a
lvwenlong <lvwenlong_lambda@qq.com>
2016-05-20 10:52:38 +0800

    Use CMake to control the macros
    the DEBUG_OPTIMIZER and WRITE_LOG optimizer

--- src/def.h
+++ src/def.h
@@ -1,7 +1,8 @@
 #pragma once
 #include<random>
+// These two macros are controlled by CMake
 // #define DEBUG_OPTIMIZER
-#define WRITE_LOG
+// #define WRITE_LOG
 
 #ifndef RAND_SEED
 #ifdef DEBUG_OPTIMIZER
@@ -10,4 +11,3 @@
 #define RAND_SEED std::random_device{}()
 #endif
 #endif
-
